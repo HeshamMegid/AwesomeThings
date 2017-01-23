43958ce7a1d13b495a0888aec146f21cf85cf425
Hesham Abd-Elmegid <hmegid@instabug.com>
2017-01-23 10:29:28 +0200

    Initial commit.

---
+++ AwesomeThingsTests/AwesomeThingsTests.swift
@@ -0,0 +1,34 @@
+//
+//  AwesomeThingsTests.swift
+//  AwesomeThingsTests
+//
+//  Created by Hesham Abd-Elmegid on 1/23/17.
+//  Copyright © 2017 Tinybits. All rights reserved.
+//
+
+import XCTest
+@testable import AwesomeThings
+
+class AwesomeThingsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testPassingTest() {
+        let text = "Hello World"
+        XCTAssertNotNil(text)
+    }
+    
+    func textFailingTest() {
+        let text = "Hello World"
+        XCTAssertNil(text)
+    }
+    
+}
